6c9e4c06b08ce09e7100db358954ac845ff1b372
miyamoto <midaisuk@gmail.com>
2018-05-06 16:43:05 +0000

    implement static version of list view and watch view

--- html/deploy.sh
+++ html/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-PROJECT_NAME=voc
 
-aws s3 cp index.html $1${PROJECT_NAME}/
-aws s3 cp watch.html $1${PROJECT_NAME}/
-aws s3 cp voc.css $1${PROJECT_NAME}/
+aws s3 cp index.html $1
+aws s3 cp watch.html $1
+aws s3 cp voc.css $1
